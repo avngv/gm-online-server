daec33e299abf8e089c36c2ac2aaab1ad9229f2a
avngv <avnguyenvan@gmail.com>
2026-01-27 22:23:01 +0900

    accept gamemaker websocket protocol

--- index.js
+++ index.js
@@ -3,29 +3,89 @@ const WebSocket = require("ws");
 
 const PORT = process.env.PORT || 8080;
 
-const server = http.createServer((req, res) =>
-{
-    res.writeHead(200);
-    res.end("OK");
+const server = http.createServer();
+
+const wss = new WebSocket.Server({
+    server,
+    handleProtocols: (protocols) =>
+    {
+        if (protocols.includes("binary")) return "binary";
+        return false;
+    }
 });
 
-const wss = new WebSocket.Server({ server });
+let players = [];
+
+function broadcast(obj)
+{
+    const msg = JSON.stringify(obj);
+    players.forEach(p =>
+    {
+        if (p.readyState === WebSocket.OPEN)
+            p.send(msg);
+    });
+}
 
 wss.on("connection", (ws) =>
 {
-    console.log("Client connected");
+    console.log("Player connected");
 
-    ws.send("hello from railway");
-
-    ws.on("message", (msg) =>
+    if (players.length >= 2)
     {
-        console.log("Received:", msg.toString());
-        ws.send("echo: " + msg);
+        ws.send(JSON.stringify({
+            type: "full"
+        }));
+        ws.close();
+        return;
+    }
+
+    players.push(ws);
+
+    // tell player to wait
+    ws.send(JSON.stringify({
+        type: "wait",
+        count: players.length
+    }));
+
+    // when 2 players connected
+    if (players.length === 2)
+    {
+        console.log("Match ready");
+
+        players[0].send(JSON.stringify({
+            type: "start",
+            playerIndex: 0
+        }));
+
+        players[1].send(JSON.stringify({
+            type: "start",
+            playerIndex: 1
+        }));
+    }
+
+    ws.on("message", (data) =>
+    {
+        let msg;
+
+        try {
+            msg = JSON.parse(data.toString());
+        }
+        catch {
+            return;
+        }
+
+        console.log("CLIENT:", msg);
     });
 
     ws.on("close", () =>
     {
-        console.log("Client disconnected");
+        console.log("Player disconnected");
+
+        players = players.filter(p => p !== ws);
+
+        broadcast({
+            type: "opponent_left"
+        });
     });
 });
 
